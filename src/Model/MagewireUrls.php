3d0c03db72a819d8a06795d4b59b72bff72c9ef2
Marcus Venghaus <marcus.venghaus@inklammern.de>
2023-04-14 13:48:51 +0200

    added di magewireurls to control which routes are handled

---
+++ src/Model/MagewireUrls.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MVenghaus\MagewirePluginPrivateContentVersion\Model;
+
+class MagewireUrls
+{
+    public function __construct(
+        private readonly array $urls
+    ) {
+    }
+
+    public function isValidRequest(string $requestUrl): bool
+    {
+        foreach ($this->urls as $url) {
+            if (str_starts_with($requestUrl, $url)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- src/Plugin/Magento/Framework/App/PageCache/VersionPlugin.php
+++ src/Plugin/Magento/Framework/App/PageCache/VersionPlugin.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace MVenghaus\MagewirePluginPrivateContentVersion\Plugin\Magento\Framework\App\PageCache;
 
 use Magento\Framework\App\PageCache\Version as Subject;
-use MVenghaus\MagewirePluginPrivateContentVersion\Helper\HttpRequestHelper;
+use Magento\Framework\App\Request\Http as HttpRequest;
+use MVenghaus\MagewirePluginPrivateContentVersion\Model\MagewireUrls;
 
 class VersionPlugin
 {
     public function __construct(
-        private readonly HttpRequestHelper $httpRequestHelper
+        private readonly HttpRequest $httpRequest,
+        private readonly MagewireUrls $magewireUrls,
     ) {
     }
 
@@ -19,7 +21,7 @@ class VersionPlugin
      */
     public function aroundProcess(Subject $subject, callable $proceed): void
     {
-        if ($this->httpRequestHelper->isMagewireRequest()) {
+        if ($this->magewireUrls->isValidRequest($this->httpRequest->getRequestUri())) {
             return;
         }
 
